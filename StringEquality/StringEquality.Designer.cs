0336f95b9cac7fd9012290cb5d5887a1e81e7528
Matt-the-who <matthew.ho@ocsbstudent.ca>
2015-11-09 14:20:54 -0500

    Finished

---
+++ StringEquality/StringEquality.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace StringEquality
+{
+    partial class frmStringEquality
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtString1 = new System.Windows.Forms.TextBox();
+            this.txtString2 = new System.Windows.Forms.TextBox();
+            this.lblText1 = new System.Windows.Forms.Label();
+            this.lblText2 = new System.Windows.Forms.Label();
+            this.mnuStrip = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.mnuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtString1
+            // 
+            this.txtString1.Location = new System.Drawing.Point(114, 44);
+            this.txtString1.Name = "txtString1";
+            this.txtString1.Size = new System.Drawing.Size(100, 20);
+            this.txtString1.TabIndex = 0;
+            // 
+            // txtString2
+            // 
+            this.txtString2.Location = new System.Drawing.Point(114, 79);
+            this.txtString2.Name = "txtString2";
+            this.txtString2.Size = new System.Drawing.Size(100, 20);
+            this.txtString2.TabIndex = 1;
+            // 
+            // lblText1
+            // 
+            this.lblText1.AutoSize = true;
+            this.lblText1.Location = new System.Drawing.Point(44, 47);
+            this.lblText1.Name = "lblText1";
+            this.lblText1.Size = new System.Drawing.Size(64, 13);
+            this.lblText1.TabIndex = 2;
+            this.lblText1.Text = "Enter text 1:";
+            // 
+            // lblText2
+            // 
+            this.lblText2.AutoSize = true;
+            this.lblText2.Location = new System.Drawing.Point(43, 82);
+            this.lblText2.Name = "lblText2";
+            this.lblText2.Size = new System.Drawing.Size(64, 13);
+            this.lblText2.TabIndex = 3;
+            this.lblText2.Text = "Enter text 2:";
+            // 
+            // mnuStrip
+            // 
+            this.mnuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mnuStrip.Name = "mnuStrip";
+            this.mnuStrip.Size = new System.Drawing.Size(262, 24);
+            this.mnuStrip.TabIndex = 4;
+            this.mnuStrip.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(90, 124);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 5;
+            this.btnCheck.Text = "Confirm";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(55, 173);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 13);
+            this.lblAnswer.TabIndex = 6;
+            // 
+            // frmStringEquality
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 229);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblText2);
+            this.Controls.Add(this.lblText1);
+            this.Controls.Add(this.txtString2);
+            this.Controls.Add(this.txtString1);
+            this.Controls.Add(this.mnuStrip);
+            this.MainMenuStrip = this.mnuStrip;
+            this.Name = "frmStringEquality";
+            this.Text = "String Comparing";
+            this.mnuStrip.ResumeLayout(false);
+            this.mnuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtString1;
+        private System.Windows.Forms.TextBox txtString2;
+        private System.Windows.Forms.Label lblText1;
+        private System.Windows.Forms.Label lblText2;
+        private System.Windows.Forms.MenuStrip mnuStrip;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ StringEquality/StringEquality.cs
@@ -0,0 +1,80 @@
+﻿/*
+Created by Matthew Ho
+Created on 11-09-15
+Created for ICS3U
+Assignment #4-06
+This program checks if two strings are the same but is not case sensitive.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringEquality
+{
+    public partial class frmStringEquality : Form
+    {
+        public bool StringEquality(string first, string second)
+        {
+            //This checks if two strings are the same but is not case sensitive.
+
+            //Variables
+            bool trueOrFalse;
+            string firstUpper;
+            string secondUpper;
+
+            firstUpper = first.ToUpper();
+            secondUpper = second.ToUpper();
+
+            //Process
+            if(firstUpper == secondUpper)
+            {
+                trueOrFalse = true;
+            }
+            else
+            {
+                trueOrFalse = false;
+            }
+            //Return value
+            return trueOrFalse;
+        }
+
+        public frmStringEquality()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            //Variables
+            string string1 = this.txtString1.Text;
+            string string2 = this.txtString2.Text;
+            bool trueOrFalse;
+
+            //Call function
+            trueOrFalse = StringEquality(string1, string2);
+
+            //Output
+            if (trueOrFalse == true)
+            {
+                this.lblAnswer.Text = "These are the same.";
+            }
+            else
+            {
+                this.lblAnswer.Text = "These are not the same.";
+            }
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            //Exit
+            this.Close();
+        }
+    }
+}
